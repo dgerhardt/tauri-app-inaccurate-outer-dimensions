93cd7f24a3b23e9a938538647e76b8ca0606df2d
Daniel Gerhardt <code@dgerhardt.net>
2024-11-01 13:04:39 +0100

    Add reproduction

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,51 @@
-import { invoke } from "@tauri-apps/api/core";
+import { PhysicalPosition, PhysicalSize } from "@tauri-apps/api/dpi";
+import { WebviewWindow } from "@tauri-apps/api/webviewWindow";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+let xEl: HTMLInputElement | null;
+let yEl: HTMLInputElement | null;
+let wEl: HTMLInputElement | null;
+let hEl: HTMLInputElement | null;
+let outerEl: HTMLInputElement | null;
+let win = WebviewWindow.getCurrent();
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
+async function updateDimensionFields() {
+  const position = await (outerEl?.checked ? win.outerPosition() : win.innerPosition());
+  const size = await (outerEl?.checked ? win.outerSize() : win.innerSize());
+  xEl!.value = position.x.toString();
+  yEl!.value = position.y.toString();
+  wEl!.value = size.width.toString();
+  hEl!.value = size.height.toString();
+}
+
+function updateWindowDimensions() {
+  win.setPosition(getPositionFromInputs());
+  win.setSize(getSizeFromInputs());
+}
+
+function getPositionFromInputs(): PhysicalPosition {
+  const x = +(xEl?.value ?? 0);
+  const y = +(yEl?.value ?? 0);
+  return new PhysicalPosition(x, y);
+}
+
+function getSizeFromInputs(): PhysicalSize {
+  const w = +(wEl?.value ?? 0);
+  const h = +(hEl?.value ?? 0);
+  return new PhysicalSize(w, h);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+  xEl = document.querySelector("#x");
+  yEl = document.querySelector("#y");
+  wEl = document.querySelector("#w");
+  hEl = document.querySelector("#h");
+  outerEl = document.querySelector("#outer");
+  document.querySelector("#dimensions-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
-    greet();
+    updateWindowDimensions();
+  });
+  document.querySelector("#dimensions-get-button")?.addEventListener("click", (e) => {
+    e.preventDefault();
+    updateDimensionFields();
   });
 });
